402370c01992b5b5ffc8a4d2343c375dbabbbf37
mayday1213 <2823129182@qq.com>
2024-11-25 20:59:25 +0800

    Site updated: 2024-11-25 20:59:25

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732539030433";
+const VERSION = "1732539555524";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
